c026a11ebd630bea09b35b1c1b700808eb5a20bf
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-01 12:07:56 -0500

    code initialization for marsden

---
+++ tests/test_convert_A.py
@@ -0,0 +1,36 @@
+# Local imports
+# -----------------------
+from os.path import join, dirname, abspath, isfile
+
+import sys
+pack_dir  = dirname(dirname(abspath(__file__))) # Package directory
+code_dir  = join(pack_dir, 'mpc_orb')
+sys.path.append(code_dir)
+
+import schema
+import filepaths
+import interpret
+
+import convert
+
+# Tests
+# -----------------------
+def test_convert_A():
+    """
+    Test the conversion from orbfit-json (lots of strings) to mpc_orb format
+    NB. This is ONLY TESTING THE INTERNAL ROUNTE *std_format_els* AT THIS POINT
+    """
+
+    # interpret the input (allow dict or filepath)
+    orbfit_dict, input_filepath = interpret.interpret( filepaths.schema_name_dict['orbfit_defining_sample']  )
+
+    # check the input is valid
+    schema.validate_orbfit(orbfit_dict)
+
+    # do the conversion (this is the heart of the routine)
+    standard_format_dict = convert.std_format_els(orbfit_dict)
+
+    # Just assert that we have a dict for now
+    assert isinstance(standard_format_dict , dict )
+
+
