a8d069738a01e701362d4d55de104cde7e5ba6e1
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-02 14:29:47 -0500

    code initialization for marsden

--- mpc_orb/bootstrap.py
+++ mpc_orb/bootstrap.py
@@ -23,8 +23,17 @@ schema.create_orbfit_felfile_schema_from_defining_sample_json()
 
 
 # (2) Convert "orbfit felfile" defining sample(s) to create defining "mpcorb" defining sample(s)
-mpcorb_format_dict = convert.std_format_els(orbfit_dict)
-schema.save_json(filepaths.schema_name_dict['mpcorb_defining_sample'], mpcorb_format_dict)
+filepaths = filepath_dict['orbfit_defining_sample']
+for fp in filepaths:
+    print(fp)
+    # Get the file contents
+    orbfit_dict,input_filepath = interpret.interpret(fp)
+    # Validate (unnecessary)
+    schema.validate_orbfit_conversion(orbfit_dict)
+    # Convert to mpc_orb format
+    mpcorb_format_dict = convert.std_format_els(orbfit_dict)
+    # Save to file
+    schema.save_json(filepaths.schema_name_dict['mpcorb_defining_sample'], mpcorb_format_dict)
 
 
 # (3) Create "mpcorb" schema from defining sample(s)
