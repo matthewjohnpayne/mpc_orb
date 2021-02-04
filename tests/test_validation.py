c162f65ddbf2105b1adf2bb1f7a2c2aba7733180
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-04 09:46:27 -0500

    test code

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -29,10 +29,48 @@ values_for_each_test   = [
 @pytest.mark.parametrize( names_of_variables , values_for_each_test )
 def test_validation_orbfit_general_A( dictionary_key_for_filepaths ):
     '''
-    Test the validation of general orbfit jsons (string version)
+    Test the validation of GENERAL orbfit jsons (string version)
     These may or may not be suitable for conversion to mpcorb (those are tested separately)
     '''
   
-    for f in filepath_dict['test_pass_orbfit_general']:
+    for f in filepath_dict[dictionary_key_for_filepaths]:
+        schema.validate_orbfit_general(f)
+
+
+
+
+names_of_variables     = ('dictionary_key_for_filepaths')
+values_for_each_test   = [
+    ('test_pass_orbfit_convert'),
+    pytest.param('test_fail_orbfit_convert',
+                 marks=pytest.mark.xfail(reason='Expected fail, invalid file'))
+]
+@pytest.mark.parametrize( names_of_variables , values_for_each_test )
+def test_validation_orbfit_general_A( dictionary_key_for_filepaths ):
+    '''
+    Test the validation of CONVERT orbfit jsons (string version)
+    These are a subset of orbfit_general
+    '''
+  
+    for f in filepath_dict[dictionary_key_for_filepaths]:
+        schema.validate_orbfit_general(f)
+
+
+
+
+
+names_of_variables     = ('dictionary_key_for_filepaths')
+values_for_each_test   = [
+    ('test_pass_mpcorb'),
+    pytest.param('test_fail_mpcorb',
+                 marks=pytest.mark.xfail(reason='Expected fail, invalid file'))
+]
+@pytest.mark.parametrize( names_of_variables , values_for_each_test )
+def test_validation_orbfit_general_A( dictionary_key_for_filepaths ):
+    '''
+    Test the validation of MPCORB jsons (numerical jsons)
+    '''
+  
+    for f in filepath_dict[dictionary_key_for_filepaths]:
         schema.validate_orbfit_general(f)
 
