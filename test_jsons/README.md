b43c259e3c75770b378882b3b9d64909a8bf72e6
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-04 08:59:01 -0500

    test json file-structure

---
+++ test_jsons/README.md
@@ -0,0 +1,8 @@
+# JSON Tests
+
+ - This directory should hold all of the jsons we want to use as tests
+
+ - The sub-directories should have obvious names:
+ - 3 "pass" sub-dirs for the 3 different schema 
+ - 3 "fail" sub-dirs for the 3 different schema
+
--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -5,7 +5,7 @@ Test related to orbfit-json
 
 # Local imports
 # -----------------------
-from os.path import join, dirname, abspath, isfile
+from os.path import join, dirname, abspath, isfile,
 
 import sys
 pack_dir  = dirname(dirname(abspath(__file__))) # Package directory
@@ -18,26 +18,35 @@ import filepaths
 
 # Tests
 # -----------------------
-def test_create_orbfit_felfile_schema_from_defining_sample_json_A():
+def test_bootstrap_A():
+    '''
+    Test the "bootstrap" routine that takes us from zero-to-hero
+    I.e. the bootstrap function starts from some defining orbfit jsons (str-format)
+    and takes us all the way to having the three desired sets of json schema
+    '''
 
-    # Assert that the schema file does NOT exist
-    assert not isfile( filepaths.schema_name_dict['orbfit_schema']  )
+    # Assert that the required defining input files exist
+    assert len(filepath_dict['orbfit_defining_sample']) > 5 ,\
+        'Insufficient files: {filepath_dict['orbfit_defining_sample']}'
+    for f in filepath_dict['orbfit_defining_sample']:
+        assert isfile( f ), 'file {f} does not exist'
     
-    # Assert that the required defining input file DOES exist
-    assert isfile( filepaths.schema_name_dict['orbfit_defining_sample']  )
+    # Explicitly delete any of the schema files and/or "numerical conversion" files ...
+    # that have previously been generated from the above defining samples
+    for f in filepath_dict['mpcorb_defining_sample']:
+        os.path.remove(f)
+    for f in ['orbfit_general_schema','orbfit_conversion_schema', 'mpcorb_schema']:
+        os.path.remove(filepath_dict[f])
 
-    # Run the code to create the schema from the defining sample json
-    schema.create_orbfit_felfile_schema_from_defining_sample_json()
+    # Run the bootstap code to create ...
+    # orbfit schema
+    # converted files (str -to- num) to act as defining mpcorb files
+    # mpcorb schema
+    bootstrap.bootstrap()
 
-    # Assert that the schema file exists
-    assert isfile( filepaths.schema_name_dict['orbfit_schema']  )
+    # Assert that the required files now exist
+    for f in filepath_dict['mpcorb_defining_sample']:
+        assert isfile(f) , '{f} does not exist'
+    for f in ['orbfit_general_schema','orbfit_conversion_schema', 'mpcorb_schema']:
+        assert isfile(filepath_dict[f]), '{filepath_dict[f]} does not exist'
 
-
-def test_validate_orbfit_A():
-    """ Test that the defining sample passes validation (it really should!!!) """
-
-    # Input filepath
-    f = filepaths.schema_name_dict['orbfit_defining_sample']
-        
-    # Validate
-    schema.validate_orbfit( f )
