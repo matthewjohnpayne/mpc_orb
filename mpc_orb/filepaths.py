5cae6ce812926e1993e0a4a48e9e9dcd0268771b
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-04 09:03:58 -0500

    test code

--- mpc_orb/filepaths.py
+++ mpc_orb/filepaths.py
@@ -12,7 +12,7 @@ from os.path import join, dirname, abspath
 pack_dir  = dirname(dirname(abspath(__file__))) # Package directory
 def_dir  = join(pack_dir, 'defining_sample_json')
 sch_dir  = join(pack_dir, 'schema_json')
-tmp_dir  = join(pack_dir, 'dev_json')
+tst_dir  = join(pack_dir, 'test_jsons')
 
 # start with defining felfiles ...
 orbfit_defining_files      = glob.glob( def_dir + "/*str.json" )
@@ -25,6 +25,14 @@ filepath_dict = {
     'orbfit_general_schema'     : join(sch_dir, 'orbfit_general_schema.json'),
     'orbfit_conversion_schema'  : join(sch_dir, 'orbfit_conversion_schema.json'),
     'mpcorb_schema'             : join(sch_dir, 'mpcorb_schema.json'),
+
+    'test_fail_mpcorb'          : glob.glob( tst_dir + "/fail_mpcorb/*" ),
+    'test_fail_orbfit_convert'  : glob.glob( tst_dir + "/fail_orbfit_convert/*" ),
+    'test_fail_orbfit_general'  : glob.glob( tst_dir + "/fail_orbfit_general/*" ),
+    'test_pass_mpcorb'          : glob.glob( tst_dir + "/pass_mpcorb/*" ),
+    'test_pass_orbfit_convert'  : glob.glob( tst_dir + "/pass_orbfit_convert/*" ),
+    'test_pass_orbfit_general'  : glob.glob( tst_dir + "/pass_orbfit_general/*" ),
+
 }
 
 
