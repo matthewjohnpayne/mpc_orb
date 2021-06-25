4edd8fd71d80094196d1677debafc8fdf248ab5e
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-06-25 11:42:57 -0400

    updating demos

--- demos/demo_parse.py
+++ demos/demo_parse.py
@@ -1,34 +1,34 @@
 
-# Import the MPCORB class from the parse module in the mpc_orb directory ...
+# Ensure the code-directory is in the path
 from os.path import join, dirname, abspath
-
 import sys
 pack_dir  = dirname(abspath('')) # Package directory
 code_dir  = join(pack_dir, 'mpc_orb')
 sys.path.append(code_dir)
 
+
+# Import the MPCORB class from the parse module in the mpc_orb directory ...
 from parse import MPCORB
 
-
+# Import the convenience filepath-defn dictionary
+from filepaths import filepath_dict
 
 ### Define a filepath to an example json file in the mpcorb format
-import glob
-jsn_dir  = join(pack_dir, 'test_jsons', 'pass_mpcorb')
-filepath = glob.glob(jsn_dir + '/*json')[0]
-print('filepath=\n', filepath)
-
-
+filepath = filepath_dict['test_pass_mpcorb'][0]
+print(f'filepath=\n {filepath} \n')
 
 ### Instantiate an MPCORB object & use it to parse the above json file
 ### NB The parsing is done by default "behind-the-scenes" upon instantiation
 M = MPCORB(filepath)
 
 # Demonstrate the available variables
+print('-'*22)
 print('\n Top level variables in MPCORB dictionary ... ')
 for k,v in M.__dict__.items():
-    print(k,v)
+    print(f'\t{k} : {v}\n')
 
 # Demonstrate the key-value pairs available in the "COM" coordinate dictionary
+print('-'*22)
 print('\n Variables in COM dictionary ... ')
 for k,v in M.COM.items():
-    print(k,v)
+    print(f'\t{k} : {v}\n')
